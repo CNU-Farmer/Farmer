ffebb23034aad1ab111b5af8ccd1abc0eac02319
ryeeun <dmsdl7531@naver.com>
2023-06-01 16:31:10 +0900

    feat : 레이아웃 수정

--- src/RegistState/RegistState.jsx
+++ src/RegistState/RegistState.jsx
@@ -7,12 +7,14 @@ import plant from './img/plant.png';
 
 // 테스트 끝나면 확인 이미지 태그 제거
 const RegistState = function () {
+    const [selectedImg, setSelectedImg] = useState(false);
     const showImg = (input) => {
         console.log(input);
         if(input.files && input.files[0]) {
             const reader = new FileReader();
             reader.onload = (e) => {
                 document.getElementById('test').src = e.target.result;
+                setSelectedImg(true);
             };
             reader.readAsDataURL(input.files[0]);
         } else {
@@ -24,14 +26,14 @@ const RegistState = function () {
             <Header text="현재 상태 등록"/>
             <div className="regist-state-body">
                 <img className="regist-state-body__img" src={plant} alt="plant" />
-                <span className="regist-state-body__text">작물을 촬영하여 현재 상태를 알려주세요.</span>
+                <span className="regist-state-body__text">작물을 위에서 아래 방향으로 촬영하여 <br/> 현재 상태를 알려주세요.</span>
                 <div className="regist-state-body__btn">
                     <label for="file">사진 찍기</label>
                     <input type="file" capture="environment" accept="image/*" id="file" onChange={(e) => showImg(e.target)}/>
                 </div>
                 <img id="test" className="test_img" alt="selected img"/>
             </div>
-            <button className="regist-state-next">다음</button>
+            <button className="regist-state-next" disabled={!selectedImg}>다음</button>
         </div>
     );
 };

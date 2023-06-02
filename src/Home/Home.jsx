9800c296dc74b52d394f9a471d0bf8a3571ca80f
ryeeun <dmsdl7531@naver.com>
2023-06-02 19:07:19 +0900

    feat : 홈 화면 slide 기능 추가

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
 import axios from "axios";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import './css/Home.css';
+import './css/Slider.css';
 import CropCard from "./components/CropCard";
 import EmptyCard from "./components/EmptyCard";
 import BottomNav from "../BottomNav/BottomNav";
@@ -9,21 +13,35 @@ import logo from './img/logo.png';
 
 const data = [
     {
-        type: '상추',
+        species: '상추',
         name: '상추도사',
-        harvestDate: '2주전',
-        waterDate: '2022.04.11' 
+        harvesting: '1주전',
+        latest_watered_date: '2022.04.11' 
+    },
+    {
+        species: '상추',
+        name: '상추도사',
+        harvesting: '2주전',
+        latest_watered_date: '2022.04.11' 
     }
 ];
 
 const Home = function () {
+    const settings = {
+        centerMode: true,
+        infinite: false,
+        centerPadding: "50px",
+        slidesToShow: 1,
+        speed: 500
+    };
     const [cropList, setCropList] = useState(data);
 
     useEffect(() => {
         axios
-            .get('https://sdi4676.pythonanywhere.com/')
+            .get(`${process.env.REACT_APP_API_KEY}/`)
             .then((response) => {
                 console.log(response)
+                // setCropList(response.data.crops);
             })
             .catch((error) => {
                 console.log('error', error);
@@ -33,11 +51,11 @@ const Home = function () {
     return (
         <div className="home">
             <img className="home-log" src={logo} alt="Farmer" />
-            <div className="home-body">
-                {cropList.length == 0 ? <EmptyCard /> : (
+            <Slider className="home-body" {...settings}>
+                {cropList.length === 0 ? <EmptyCard /> : (
                     cropList.map((e) => <CropCard crop={e} />)
                 )}
-            </div>
+            </Slider>
             <BottomNav />
         </div>
     );

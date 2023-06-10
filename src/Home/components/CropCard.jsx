d355f561b2837458bc0ed1a5580d8abd0eabc5e6
ryeeun <dmsdl7531@naver.com>
2023-06-10 18:17:01 +0900

    feat : 레이아웃 및 css 수정

--- src/Home/components/CropCard.jsx
+++ src/Home/components/CropCard.jsx
@@ -6,7 +6,15 @@ import '../css/CropCard.css';
 import lettuce from '../img/lettuce.png';
 import bar from '../img/bar.png';
 
+const nameMap = {
+    'lettuce': '상추'
+};
+
 const CropCard = function ({ crop }) {
+    const date = new Date(crop.latest_watered_date);
+    const dateFormat = (date) => {
+        return date.getFullYear() + '.' + ((date.getMonth() + 1) < 9 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1)) + '.' + (date.getDate() < 9 ? "0" + date.getDate() : date.getDate()) ;
+    }
     return (
         <div className="crop-card">
             <img className="crop-card-img" src={lettuce} alt="crop img" />
@@ -16,14 +24,18 @@ const CropCard = function ({ crop }) {
                     <div className="crop-card-info-text-box">
                         <span className="crop-card-info__text">작물 종류</span>
                         <img className="crop-card-info__divider" src={bar} alt="|" />
-                        <span className="crop-card-info__text">{crop.species}</span>
+                        <span className="crop-card-info__text">{nameMap[crop.species]}</span>
                     </div>
                     <span className="crop-card-info__name">{crop.name}</span>
-                    <span className="crop-card-info__harvest">{`수확 ${crop.harvesting}`}</span>
+                    <div className="crop-card-info__harvest">
+                        <span className="crop-card-info__harvest-sm">수확&nbsp;&nbsp;&nbsp;</span>
+                        <span className="crop-card-info__harvest-bg">{crop.harvesting === null ? 0 : crop.harvesting}</span>
+                        <span className="crop-card-info__harvest-sm">일 전</span>
+                    </div>
                     <div className="crop-card-info-text-box">
                         <span className="crop-card-info__text">다음 물 줄 날짜</span>
                         <img className="crop-card-info__divider" src={bar} alt="|" />
-                        <span className="crop-card-inf__water">{crop.latest_watered_date}</span>
+                        <span className="crop-card-inf__water">{dateFormat(date)}</span>
                     </div>
                     <Link to="/state" className="crop-card-state-btn">상태 등록</Link>
                 </div>
